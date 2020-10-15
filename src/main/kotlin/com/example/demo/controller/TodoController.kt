3886fee62f35fabf01df385aba632d09d84b86a8
whistleJs <whistle_js@naver.com>
2020-10-15 22:03:26 +0900

    2020-10-15 TodoController 작업 [Create]

--- src/main/kotlin/com/example/demo/controller/TodoController.kt
+++ src/main/kotlin/com/example/demo/controller/TodoController.kt
@@ -1,9 +1,19 @@
 package com.example.demo.controller
 
+import com.example.demo.model.entity.TodoEntity
+import com.example.demo.model.response.TodoResponse
+import com.example.demo.service.TodoService
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/todo")
-class TodoController {
+class TodoController(
+        var todoService: TodoService
+) {
+    @PostMapping
+    fun create(todoResponse: TodoResponse): TodoEntity {
+        return todoService.create(todoResponse)
+    }
 }
