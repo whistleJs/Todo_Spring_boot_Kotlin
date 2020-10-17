e3e1dc6f498a9ad5b2827967d33e215ce96174db
whistleJs <whistle_js@naver.com>
2020-10-17 18:00:25 +0900

    2020-10-17 TodoRepository - 작업 [FindByTitleLike 추가]

--- src/main/kotlin/com/example/demo/repository/TodoRepository.kt
+++ src/main/kotlin/com/example/demo/repository/TodoRepository.kt
@@ -3,4 +3,6 @@ package com.example.demo.repository
 import com.example.demo.model.entity.TodoEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TodoRepository: JpaRepository<TodoEntity, Long>
+interface TodoRepository: JpaRepository<TodoEntity, Long> {
+    fun findByTitleLike(keyword: String): List<TodoEntity>
+}
