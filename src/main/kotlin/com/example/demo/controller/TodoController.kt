8b705ec3827fe7a0bfd4c1b2fb285aa683144c56
whistleJs <whistle_js@naver.com>
2020-10-17 18:01:09 +0900

    2020-10-17 TodoController - 작업 [FindByTitle Like]

--- src/main/kotlin/com/example/demo/controller/TodoController.kt
+++ src/main/kotlin/com/example/demo/controller/TodoController.kt
@@ -20,6 +20,11 @@ class TodoController(
         return todoService.findById(id)
     }
 
+    @GetMapping("/search/{keyword}")
+    fun findByKeyword(@PathVariable keyword: String): List<TodoEntity> {
+        return todoService.findByKeyword(keyword)
+    }
+
     @PostMapping
     fun create(@RequestBody todoResponse: TodoResponse): TodoEntity {
         return todoService.create(todoResponse)
