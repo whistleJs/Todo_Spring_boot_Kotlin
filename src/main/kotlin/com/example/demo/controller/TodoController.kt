8ea5c1e24b5409faf7d1b762627096bd4867ea9e
whistleJs <whistle_js@naver.com>
2020-10-17 09:44:02 +0900

    2020-10-17 TodoController - 작업 [FindAll]

--- src/main/kotlin/com/example/demo/controller/TodoController.kt
+++ src/main/kotlin/com/example/demo/controller/TodoController.kt
@@ -3,16 +3,18 @@ package com.example.demo.controller
 import com.example.demo.model.entity.TodoEntity
 import com.example.demo.model.response.TodoResponse
 import com.example.demo.service.TodoService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/todo")
 class TodoController(
         var todoService: TodoService
 ) {
+    @GetMapping
+    fun findAll(): List<TodoEntity> {
+        return todoService.findAll()
+    }
+
     @PostMapping
     fun create(@RequestBody todoResponse: TodoResponse): TodoEntity {
         return todoService.create(todoResponse)
