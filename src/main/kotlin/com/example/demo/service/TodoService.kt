95b9069e1a7282e7f4307172fc8385e8b5b01041
whistleJs <whistle_js@naver.com>
2020-10-17 16:35:27 +0900

    2020-10-17 TodoService - 작업 [findById]

--- src/main/kotlin/com/example/demo/service/TodoService.kt
+++ src/main/kotlin/com/example/demo/service/TodoService.kt
@@ -4,6 +4,7 @@ import com.example.demo.model.entity.TodoEntity
 import com.example.demo.model.response.TodoResponse
 import com.example.demo.repository.TodoRepository
 import org.springframework.stereotype.Service
+import javax.persistence.NoResultException
 
 @Service
 class TodoService(
@@ -12,6 +13,12 @@ class TodoService(
     fun findAll(): List<TodoEntity> {
         return todoRepository.findAll()
                 .filter { t: TodoEntity -> t.deletedAt == null }
+    }
+
+    fun findById(id: Long): TodoEntity {
+        return todoRepository.findById(id)
+                .filter { t: TodoEntity -> t.deletedAt == null }
+                .orElseThrow { NoResultException("No result id : " + id) }
     }
 
     fun create(response: TodoResponse): TodoEntity {
